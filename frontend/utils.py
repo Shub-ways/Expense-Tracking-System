f16c39fa88e476e596b436aa454313a4f90d5e99
Shubham kumar <shubhamkumar44838@gmail.com>
2026-05-25 01:37:09 +0530

    fix: load API_URL from environment dynamically in utils.py

--- frontend/utils.py
+++ frontend/utils.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-API_URL = "http://localhost:8000"
+import os
+
+API_URL = os.getenv("API_URL", "http://localhost:8000")
 
 def get_auth_headers():
     """Return headers containing the Bearer token if the user is logged in."""
