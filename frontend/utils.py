d7aebcc423ad5542e6777b69268be7e6170598b4
Shubham kumar <shubhamkumar44838@gmail.com>
2026-05-25 01:42:31 +0530

    Fix frontend healthcheck and fallback API URL

--- frontend/utils.py
+++ frontend/utils.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 import os
 
-API_URL = os.getenv("API_URL", "http://localhost:8000")
+API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
 
 def get_auth_headers():
     """Return headers containing the Bearer token if the user is logged in."""
